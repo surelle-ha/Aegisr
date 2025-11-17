77dff31e25653fe78dc00e88957819cffd880dfc
Harold Eustaquio <eustahc@realcognita.com>
2025-11-17 11:29:55 +0800

    feat(na): added get, put, delete and clear commands (#5)
    * feat(na): repl support initiated
    
    * feat(na): added get, put, delete and clear commands

--- benches/engine_bench.rs
+++ benches/engine_bench.rs
@@ -1,7 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use aegisr_engine::{AegFileSystem, AegCore as EngineCore};
 
 fn benchmark_put_get(c: &mut Criterion) {
+    // Ensure clean environment
     AegFileSystem::reset_files();
     AegFileSystem::initialize_config(None, None);
 
@@ -10,10 +10,11 @@ fn benchmark_put_get(c: &mut Criterion) {
 
     c.bench_function("put_value", |b| {
         b.iter(|| {
+            // repeatedly put the same key/value
             EngineCore::put_value(key, value);
         })
     });
-    
+
     EngineCore::put_value(key, value);
 
     c.bench_function("get_value", |b| {
